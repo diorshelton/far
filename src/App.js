7d9e7af9f5f1149e44b316342e353000e3dc17a3
Dior.Shelton <dior.shelton@gmail.com>
2024-03-05 15:52:54 -0500

    Remove header module and all references in favor of Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './header/header';
 import Card from "./card/Card.jsx";
 import Layout from './components/Layout';
 import { bodies } from './mockData/mockData';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div>
       <Layout></Layout>
-      {/* <Header /> */}
       {/* <Card data={bodies} /> */}
     </div>
   );
