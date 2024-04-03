9830718daae7799ce1c0d42ef91a5906111eaa1f
Dior.Shelton <dior.shelton@gmail.com>
2024-04-03 13:56:29 -0400

    Add dummy cards to create/test Grid.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Card from "./components/Card.jsx";
 import Layout from "./components/Layout";
 import {bodies} from "./mockData/mockData.js"
+import Grid from "./components/Grid.jsx";
 
 function App() {
 	// const [data, setData] = useState([]);
@@ -20,17 +21,41 @@ function App() {
 	return (
 		<>
 			<Layout>
-				<Card bodyName={bodies[0].englishName} bodyType={bodies[0].bodyType} volume={bodies[0].v} density={bodies[0].density} mass={bodies[0].mass.massValue} massExponent={bodies[0].mass.massExponent}
-				/>
-				{/* {bodies.map(body =>
-					<Card key={body.id} bodyName={body.englishName}
-						bodyType={body.bodyType}
-						volume={body.vol}
-						density={body.density}
-						mass={body.mass}
-					>
-					</Card>
-				)} */}
+
+				<Grid>
+					<Card
+						bodyName={bodies[0].englishName}
+						bodyType={bodies[0].bodyType}
+						volume={bodies[0].v}
+						density={bodies[0].density}
+						mass={bodies[0].mass.massValue}
+						massExponent={bodies[0].mass.massExponent}
+					/>
+					<Card
+						bodyName={bodies[1].englishName}
+						bodyType={bodies[1].bodyType}
+						volume={bodies[1].v}
+						density={bodies[1].density}
+						mass={bodies[1].mass.massValue}
+						massExponent={bodies[1].mass.massExponent}
+					/>
+					<Card
+						bodyName={bodies[2].englishName}
+						bodyType={bodies[2].bodyType}
+						volume={bodies[2].v}
+						density={bodies[2].density}
+						mass={bodies[2].mass.massValue}
+						massExponent={bodies[2].mass.massExponent}
+					/>{" "}
+					<Card
+						bodyName={bodies[3].englishName}
+						bodyType={bodies[3].bodyType}
+						volume={bodies[3].v}
+						density={bodies[3].density}
+						mass={bodies[3].mass.massValue}
+						massExponent={bodies[3].mass.massExponent}
+					/>
+				</Grid>
 			</Layout>
 		</>
 	);
---
+++ src/components/Grid.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledGrid = styled.div`
+	.container {
+		background-color: #cea37d;
+		display: grid;
+		place-items: center;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+	}
+	.contents {
+		/* justify-self: center; */
+	}
+`;
+
+const Grid = ({ children }) => {
+	return (
+		<StyledGrid>
+			<div className="container">
+				<main className="contents">{children}</main>
+			</div>
+		</StyledGrid>
+	);
+};
+
+export default Grid;
